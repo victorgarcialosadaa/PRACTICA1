2ae23ebf3e1cf8011cc25768df9c75b9d5d14bb5
victor <victor@victors-Mac.local>
2018-11-03 05:17:32 -0700

    prueba2

--- PRACTICA1/ViewController.swift
+++ PRACTICA1/ViewController.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        <#code#>
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        <#code#>
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        <#code#>
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        <#code#>
+    }
+    
 
+    @IBOutlet weak var pickerView: UIPickerView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        pickerView.delegate = self  
+        pickerView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
 }
+
 
---
+++ PRACTICA1/coin.swift
@@ -0,0 +1,19 @@
+//
+//  coin.swift
+//  PRACTICA1
+//
+//  Created by victor on 03/11/2018.
+//  Copyright © 2018 victor. All rights reserved.
+//
+
+import UIKit
+
+class Coin {
+    var name: String
+    var value: Decimal
+    init(name: String, value: Decimal) {self.name = name
+        self.value = value
+        <#statements#>
+    }
+
+}
