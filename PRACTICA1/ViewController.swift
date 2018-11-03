60a1276e25c4a224b1e312f92384cd3ef137eeb4
victor <victor@victors-Mac.local>
2018-11-03 03:16:36 -0700

    Initial Commit

---
+++ PRACTICA1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PRACTICA1
+//
+//  Created by victor on 03/11/2018.
+//  Copyright © 2018 victor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ PRACTICA1Tests/PRACTICA1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  PRACTICA1Tests.swift
+//  PRACTICA1Tests
+//
+//  Created by victor on 03/11/2018.
+//  Copyright © 2018 victor. All rights reserved.
+//
+
+import XCTest
+@testable import PRACTICA1
+
+class PRACTICA1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
